e25dda0e3fb8666c5a9ac995c11f2fdf70f878be
Kateryna Sokur <k.o.sokur@gmail.com>
2023-07-28 13:54:48 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
