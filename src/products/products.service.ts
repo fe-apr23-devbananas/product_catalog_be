2dc815b3de8e5efa5542226879221fc0fdd0bb09
Oleksii Lysenko <nano77@ukr.net>
2023-08-08 13:28:09 +0300

    test findNewest

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -35,7 +35,7 @@ export class ProductsService {
   findNewest() {
     return Products.findAll({
       limit: 10,
-      order: [['year', 'DESC']],
+      order: [['name', 'DESC']],
     });
   }
 }
